f7db38134db0ff92cf6745d82df97725a5e8a6e3
Adwait Hegde <adwait.hegde@spit.ac.in>
2021-11-20 00:05:18 +0530

    forms and static files added

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .tasks import send_mail_func
-
+from .forms import NoticeBoardForm
 
 # Create your views here.
 def test(request):
     # send_mail_func.delay()
-    return HttpResponse("Sent!")
+    # return HttpResponse("Sent!")
+    form = NoticeBoardForm()
+    return render(request, 'app/index.html', {'form': form})
